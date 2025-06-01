a1655124a778b6f95eb2497aca1a805d70fea9d1
aarvoa <aaravsreekanth@outlook.com>
2025-06-01 12:02:15 +0200

    checking in

--- forloop.py
+++ forloop.py
@@ -5,5 +5,18 @@ x,y,z= ms.player_position()
 x,y,z= int(x),int(y),int(z)
 
 for i in range(0,10):
-    ms.execute(f"setblock {x+i} {y} {z} oak_planks")
+    if i%2 ==0:
+        ms.execute(f"setblock {x+i} {y} {z} oak_planks")
+    elif i%5 == 0:
+        ms.execute(f"setblock {x + i} {y} {z} diamond_block")
+    else:
+        ms.execute(f"setblock {x + i} {y} {z} netherite_block")
+
+for i in range(0,10):
+    if i%2 ==0:
+        ms.execute(f"setblock {x} {y+i} {z} oak_planks")
+    elif i%5 == 0:
+        ms.execute(f"setblock {x} {y+i} {z} diamond_block")
+    else:
+        ms.execute(f"setblock {x} {y+i} {z} netherite_block")
 ms.echo("program is done")
