544271bc0d628f0ef7a5f44e7cc5cccc801eda65
aarvoa <aaravsreekanth@outlook.com>
2025-06-10 21:24:11 +0200

    checking in

--- calculator.py
+++ calculator.py
@@ -8,21 +8,36 @@ print("3.Multiply")
 print("4.Divide")
 
 opt = input("Enter the number of your choice (1/2/3/4): ")
+if int(opt) > 4:
+    print("Oops. Invalid Option")
+    exit(0)
 
 num1 = int(input("First Number: "))
 num2 = int(input("Second Number: "))
+
+
 
 def add(a, b):
     return a + b
 
 def subtract(a, b):
+    check_nums(a,b)
     return a - b
+
+
+def check_nums(num1,num2):
+    if num1 < num2:
+        print("Oops. The First Number is smaller than the Second Number")
+        exit(0)
+
 
 def multiply(a, b):
     return a * b
 
 def divide(a, b):
+    check_nums(a,b)
     return a / b
+
 
 if opt == "1":
     ans = add(num1,num2)
