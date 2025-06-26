a47552341f05ef78dac0c3a3de3b0b9190be214c
aarvoa <aaravsreekanth@outlook.com>
2025-06-26 20:15:13 +0200

    checking in

---
+++ apple.py
@@ -0,0 +1,9 @@
+j = input("type your name-")
+l= len(j)
+#print(l)
+
+if l%2 == 1:
+    print("oops you lose")
+else:
+    print("yay you win")
+
---
+++ oiiiaioiai.py
@@ -0,0 +1,8 @@
+num=0
+num1=1
+if num==0:
+    for i in range(0,19):
+        print("01101000 01100101 01101100 01101100 01101111 00001010")
+
+else:
+    print("hello")
